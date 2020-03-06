360b107d43a9513f58d03698dc2dde2e96e51a6f
nuroldfahmi <nuroldfahmi@gmail.com>
2020-03-06 16:15:58 +0700

    deploy

---
+++ precache-manifest.e32bbd26beec7f471719cb966c5155f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1612b7d2e8fa820e92355d1dcc801bd5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5585b118a907c47c554d",
+    "url": "/static/css/main.b440d171.chunk.css"
+  },
+  {
+    "revision": "6edd6a9796db832fe234",
+    "url": "/static/js/2.98efb424.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/static/js/2.98efb424.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5585b118a907c47c554d",
+    "url": "/static/js/main.b0a0de6e.chunk.js"
+  },
+  {
+    "revision": "a660c486894e8586f065",
+    "url": "/static/js/runtime-main.b7a6c8a9.js"
+  }
+]);
